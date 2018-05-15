e761138c0f2cc3bebb8e7bcffef8a33b25aa5646
kut-arika <kut.arika.1114@gmail.com>
2018-05-15 13:37:00 +0900

    - disable axios proxy setting

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,11 @@
 import * as axios from 'axios'
 
-let options = {}
+let options = {
+  proxy: false
+}
 // The server-side needs a full url to works
 if (process.server) {
-  options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`
+  options.baseURL = `http://${process.env.HOST || '127.0.0.1'}:${process.env.PORT || 3000}`
 }
 
 export default axios.create(options)
