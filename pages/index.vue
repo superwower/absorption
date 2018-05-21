73c7d87f432ffd3cb418bec41ea2a7635650a285
kut-arika <kut.arika.1114@gmail.com>
2018-05-21 09:21:16 +0900

    implement add list

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,27 @@
 <template>
-  <draggable :options="{draggable:'.list', group:'list'}" class="columns section" @start="onDragStart" @end="onDragEnd">
-      <List :title="list.title"
-            :listId="list.id"
-            :cards="cards[list.id]"
-            v-for="(list, index) in lists" :key="index" class="list"/>
-      <b-icon icon="table-column-plus-before" />
-  </draggable>
+  <div>
+    <nav class="level">
+      <div class="level-left"/>
+      <div class="level-right">
+        <input class="input column" type="text" placeholder="list title here" v-model="listTitle">
+        <a class="column is-2" @click="addList">
+          <b-icon icon="table-column-plus-before" />
+        </a>
+      </div>
+    </nav>
+    <draggable :options="{draggable:'.list', group:'list'}" class="columns" @start="onDragStart" @end="onDragEnd">
+        <List :title="list.title"
+              :listId="list.id"
+              :cards="cards[list.id]"
+              v-for="(list, index) in lists" :key="index" class="list"/>
+    </draggable>
+  </div>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
 import _ from 'lodash'
+import uuid from 'uuid/v1'
 
 import axios from '~/plugins/axios'
 import List from '~/components/List.vue'
@@ -19,7 +30,8 @@ export default {
   data () {
     return {
       cards: [],
-      lists: []
+      lists: [],
+      listTitle: ''
     }
   },
   async asyncData () {
@@ -27,12 +39,12 @@ export default {
     let cardsData = await axios.get('/api/cards')
 
     let lists = _.sortBy(listsData.data, ['order'])
-
     let cards = {}
+    for (let list of lists) {
+      cards[list.id] = []
+    }
+
     _.orderBy(cardsData.data, ['listId', 'order'], ['asc', 'asc']).forEach(card => {
-      if (!cards[card.listId]) {
-        cards[card.listId] = []
-      }
       cards[card.listId].push(card)
     })
     return { cards, lists }
@@ -47,6 +59,16 @@ export default {
     List
   },
   methods: {
+    addList: function () {
+      if (this.listTitle === '') {
+        return
+      }
+      const newList = { id: uuid(), order: this.lists.length, title: this.listTitle }
+      this.lists.push(newList)
+      this.cards[newList.id] = []
+      axios.post('/api/lists', newList)
+      this.listTitle = ''
+    },
     onDragStart: function (event) {
       event.item.style.opacity = '0.4'
     },
