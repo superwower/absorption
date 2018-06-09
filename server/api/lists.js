794d2f62700544e8462f1daba23378ee36d7006f
kut_arika <kut.arika.1114@gmail.com>
2018-06-09 22:12:56 +0900

    use mongoose on list api

--- server/api/lists.js
+++ server/api/lists.js
@@ -1,42 +1,39 @@
 import { Router } from 'express'
 import _ from 'lodash'
 
-const router = Router()
+import List from '../models/list'
 
-// Mock Lists
-const lists = [
-  { id: '1', order: 1, boardId: '1', title: 'keep' },
-  { id: '2', order: 2, boardId: '1', title: 'problem' },
-  { id: '3', order: 3, boardId: '1', title: 'try' }
-]
+const router = Router()
 
 /* GET lists listing. */
 router.get('/lists', function (req, res, next) {
-  let result = lists
+  const query = {}
   if (req.query.boardId) {
-    result = _.filter(lists, { 'boardId': req.query.boardId })
+    query.boardId = req.query.boardId
   }
-  res.json(result)
+  List.find(query).exec().then(lists => {
+    return res.json(lists)
+  })
 })
 
 /* POST list.  */
 router.post('/lists', function (req, res, next) {
-  lists.push(req.body)
-  res.sendStatus(200)
+  List.create(req.body).then(() => {
+    return res.sendStatus(200)
+  })
 })
 
 /* PUT list.  */
 router.put('/lists/:id', function (req, res, next) {
-  let index = _.findIndex(lists, ['id', req.params.id])
-  lists[index] = req.body
-  res.sendStatus(200)
+  List.where({ id: req.params.id }).update(req.body).exec().then(() => {
+    return res.sendStatus(200)
+  })
 })
 
 /* DELETE list.  */
 router.delete('/lists/:id', function (req, res, next) {
-  _.remove(lists, function (list) {
-    return list.id === req.params.id
+  List.deleteOne({ id: req.params.id }).then(() => {
+    return res.sendStatus(200)
   })
-  res.sendStatus(200)
 })
 export default router
