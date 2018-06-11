c0a1e5ba6c562b7c08e86584090572380ac222b1
kut_arika <kut.arika.1114@gmail.com>
2018-06-11 20:49:56 +0900

    add graphql list api

--- server/api/graphql.js
+++ server/api/graphql.js
@@ -3,6 +3,7 @@ import graphqlHTTP from 'express-graphql'
 import { buildSchema } from 'graphql'
 
 import Board from '../models/board'
+import List from '../models/list'
 
 const router = Router()
 
@@ -12,6 +13,16 @@ const root = {
   },
   boards: () => {
     return Board.find().exec()
+  },
+  list: (args) => {
+    return List.findOne({id: args.id}).exec()
+  },
+  lists: (args) => {
+    const query = {}
+    if (args.boardId) {
+      query.boardId = args.boardId
+    }
+    return List.find(query).exec()
   }
 }
 router.use('/graphql', graphqlHTTP({
@@ -19,10 +30,18 @@ router.use('/graphql', graphqlHTTP({
     type Query {
       board(id: String!): Board
       boards: [Board]
+      list(id: String!): List
+      lists(boardId: String): [List]
     },
     type Board {
       id: String,
       order: Int,
+      title: String
+    },
+    type List {
+      id: String,
+      order: Int,
+      boardId: String,
       title: String
     }
   `),
